cba703c5fe039dd050ed0d850a839072d1c4e38f
Gabriel Brun <lydstyl@gmail.com>
2021-10-20 21:40:57 +0200

    Mock Ticker module in ticker test.

--- src/rates/ticker.test.ts
+++ src/rates/ticker.test.ts
@@ -1,43 +1,34 @@
 import Ticker from './ticker'
 
-// jest.mock('./ticker')
+const mockedBid = '1.121'
+
+jest.mock('./ticker', () => {
+    const originalTickerModule = jest.requireActual('./ticker')
+
+    return {
+        __esModule: true,
+        ...originalTickerModule,
+        default: {
+            fetchTicker: jest.fn((_exchangeId: string, symbols: string) =>
+                Promise.resolve({
+                    symbol: symbols,
+                    info: {
+                        bid: mockedBid,
+                    },
+                }),
+            ),
+        },
+        //hello: 'mocked hello (export const hello = "world"',
+    }
+})
 
 describe('fetchTicker function', () => {
     it('return an object with a fetchTicker property', async () => {
-        // Ticker.fetchTicker.mockResolvedValue({});
-
-        // Ticker.fetchTicker.mockImplementation(('bitstamp', 'EUR/USD') =>
-        //     Promise.resolve({
-        //         symbol: 'string',
-        //         info: {
-        //             bid: 'string',
-        //         },
-        //     }),
-        // )
-
         const result = await Ticker.fetchTicker('bitstamp', 'EUR/USD')
 
         expect(result).toHaveProperty('symbol')
         expect(result).toHaveProperty('info')
         expect(result.info).toHaveProperty('bid')
+        expect(result.info.bid).toBe(mockedBid)
     })
 })
-
-// Mock exemple from documentation https://jestjs.io/docs/mock-functions :
-
-// // users.test.js
-// import axios from 'axios';
-// import Users from './users';
-
-// jest.mock('axios');
-
-// test('should fetch users', () => {
-//   const users = [{name: 'Bob'}];
-//   const resp = {data: users};
-//   axios.get.mockResolvedValue(resp);
-
-//   // or you could use the following depending on your use case:
-//   // axios.get.mockImplementation(() => Promise.resolve(resp))
-
-//   return Users.all().then(data => expect(data).toEqual(users));
-// });
