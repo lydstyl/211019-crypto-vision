6ddf3a26a4d305c20870762b2e6c046f7329a1f9
Gabriel Brun <lydstyl@gmail.com>
2021-10-20 21:26:50 +0200

    Mock getEURUSD.

--- src/maint.test.ts
+++ src/maint.test.ts
@@ -1,5 +1,19 @@
 import { getCryptoVison } from './main'
 
+const mockedEURUSD = 1.123
+
+jest.mock('./rates/index', () => {
+    const originalModul = jest.requireActual('./rates/index')
+
+    const mockedGetEURUSD = jest.fn(() => mockedEURUSD)
+
+    return {
+        __esModule: true,
+        ...originalModul,
+        getEURUSD: mockedGetEURUSD,
+    }
+})
+
 describe('getCryptoVision function', () => {
     it('return an object with a rates property', async () => {
         const result = await getCryptoVison()
@@ -11,5 +25,11 @@ describe('getCryptoVision function', () => {
         const result = await getCryptoVison()
 
         expect(result).toHaveProperty('accounts')
+    })
+
+    it('return an object with a mocked EURUSD', async () => {
+        const result = await getCryptoVison()
+
+        expect(result.rates.EURUSD).toBe(mockedEURUSD)
     })
 })
