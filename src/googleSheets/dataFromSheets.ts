0b73a630be31713ecc94f0122bbd0cb26beb391f
Gabriel Brun <lydstyl@gmail.com>
2021-10-21 15:48:44 +0200

    Add try catch blocks and doc id environment variable.

--- src/googleSheets/dataFromSheets.ts
+++ src/googleSheets/dataFromSheets.ts
@@ -6,22 +6,36 @@ import { GoogleSpreadsheet } from 'google-spreadsheet'
 const spreadSheet = {
     getData: async () => {
         // Initialize the sheet - doc ID is the long id in the sheets URL
-        const doc = new GoogleSpreadsheet(
-            '1rrYVxvKNLXrD-eiQLOD4hc0Clpp4xPAINc5d70VOv5c',
-        )
+        const doc = new GoogleSpreadsheet(process.env.GOOGLE_DOC_ID)
 
-        // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
-        await doc.useServiceAccountAuth({
-            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY,
-        })
+        try {
+            // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+            await doc.useServiceAccountAuth({
+                client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+                private_key: process.env.GOOGLE_PRIVATE_KEY,
+            })
+        } catch (error) {
+            console.log('ERROR WHILE INITIALIZING')
+            console.log(error)
+        }
 
-        await doc.loadInfo() // loads document properties and worksheets
-
+        try {
+            await doc.loadInfo() // loads document properties and worksheets
+        } catch (error) {
+            console.log('ERROR WHILE LOADING DOC INFOS')
+            console.log(error)
+        }
         const sheet = doc.sheetsByIndex[4] // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
 
-        // read rows
-        const rows = await sheet.getRows({ limit: 6, offset: 1 }) // can pass in { limit, offset }
+        let rows
+
+        try {
+            // read rows
+            rows = await sheet.getRows({ limit: 6, offset: 1 }) // can pass in { limit, offset }
+        } catch (error) {
+            console.log('ERROR WHILE GETTING SHEET ROWS')
+            console.log(error)
+        }
 
         const cryptos = rows.map((r) => r._rawData[0]).filter((c) => c !== '')
 
@@ -29,38 +43,6 @@ const spreadSheet = {
     },
 }
 
-// const getData = async () => {
-//     // Initialize the sheet - doc ID is the long id in the sheets URL
-//     const doc = new GoogleSpreadsheet(
-//         '1rrYVxvKNLXrD-eiQLOD4hc0Clpp4xPAINc5d70VOv5c',
-//     )
-
-//     // Initialize Auth - see more available options at https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
-//     await doc.useServiceAccountAuth({
-//         client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-//         private_key: process.env.GOOGLE_PRIVATE_KEY,
-//     })
-
-//     await doc.loadInfo() // loads document properties and worksheets
-//     // console.log(doc.title)
-
-//     //   await doc.updateProperties({ title: 'renamed doc' })
-
-//     const sheet = doc.sheetsByIndex[4] // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-//     // console.log(sheet.title)
-//     // console.log(sheet.rowCount)
-
-//     // read rows
-//     const rows = await sheet.getRows({ limit: 6, offset: 1 }) // can pass in { limit, offset }
-
-//     const cryptos = rows.map((r) => r._rawData[0]).filter((c) => c !== '')
-
-//     return cryptos
-
-//     //   // adding / removing sheets
-//     //   const newSheet = await doc.addSheet({ title: 'hot new sheet!' })
-//     //   await newSheet.delete()
-// }
 ;(async function () {
     const response = await spreadSheet.getData()
     console.log(`gb🚀 ~ response`, response)
